e55c1ba5e7a3c7276837231683998cb7a8c6dfdf
aquestry <antonkonig691@gmail.com>
2025-05-10 11:23:41 +0200

    add

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    kotlin("jvm") version "2.1.20"
+    `maven-publish`
+}
+
+group = "net.eupixel"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("net.minestom:minestom-snapshots:1_21_5-2398778b46")
+    implementation("net.kyori:adventure-text-minimessage:4.20.0")
+    implementation("org.slf4j:slf4j-simple:2.0.6")
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        mavenLocal()
+    }
+}
---
+++ src/main/kotlin/vivlib.kt
@@ -0,0 +1,7 @@
+import net.minestom.server.MinecraftServer
+
+object vivlib {
+    fun init(minecraftServer: MinecraftServer) {
+        println("vivlib v0.4")
+    }
+}
