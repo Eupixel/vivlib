875ceab8385b08ba0d8b893b77fdec3c7c9d1839
aquestry <antonkonig691@gmail.com>
2025-05-11 18:01:27 +0200

    cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import com.google.protobuf.gradle.*
+
 plugins {
     kotlin("jvm") version "2.1.20"
+    id("com.google.protobuf") version "0.9.5"
 }
 
 group = "net.eupixel"
@@ -9,10 +12,27 @@ repositories {
     mavenCentral()
 }
 
+protobuf {
+    protoc { artifact = "com.google.protobuf:protoc:3.24.1" }
+    plugins {
+        id("grpc")   { artifact = "io.grpc:protoc-gen-grpc-java:1.59.0" }
+        id("grpckt") { artifact = "io.grpc:protoc-gen-grpc-kotlin:1.4.0:jdk7@jar" }
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+        }
+    }
+}
+
 dependencies {
-    implementation("net.minestom:minestom-snapshots:1_21_5-2398778b46")
-    implementation("net.kyori:adventure-text-minimessage:4.20.0")
-    implementation("org.slf4j:slf4j-simple:2.0.6")
+    implementation("io.grpc:grpc-kotlin-stub:1.4.0")
+    implementation("io.grpc:grpc-netty-shaded:1.59.0")
+    implementation("com.google.protobuf:protobuf-kotlin:3.24.1")
+
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
