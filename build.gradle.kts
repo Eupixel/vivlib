9be2531552c3da26ee26d3d9877b76f1af8e8032
aquestry <antonkonig691@gmail.com>
2025-05-18 18:51:58 +0200

    move some logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("net.minestom:minestom-snapshots:1_21_5-2398778b46")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
+    implementation("net.kyori:adventure-text-minimessage:4.20.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
 }
---
+++ src/main/kotlin/util/PrefixLoader.kt
@@ -0,0 +1,34 @@
+package util
+
+import net.eupixel.vivlib.util.Permissions
+import net.minestom.server.coordinate.Pos
+import net.minestom.server.entity.Entity
+import net.minestom.server.entity.EntityType
+import net.minestom.server.entity.Player
+import net.kyori.adventure.text.minimessage.MiniMessage.miniMessage
+import net.minestom.server.entity.metadata.display.AbstractDisplayMeta
+import net.minestom.server.entity.metadata.display.TextDisplayMeta
+import kotlin.apply
+import kotlin.collections.filter
+import kotlin.collections.forEach
+
+object PrefixLoader {
+    fun loadPrefix(player: Player) {
+        player.passengers.filter { it.entityType == EntityType.TEXT_DISPLAY }.forEach {
+            it.remove()
+        }
+        val name = Permissions.getPrefix(player.uuid) + " " + player.username
+        val component = miniMessage().deserialize(name)
+        val display = Entity(EntityType.TEXT_DISPLAY).apply {
+            setInstance(player.instance, player.position)
+            setNoGravity(true)
+            val meta = entityMeta as TextDisplayMeta
+            meta.text = component
+            meta.billboardRenderConstraints = AbstractDisplayMeta.BillboardConstraints.CENTER
+            meta.translation = Pos(0.0, 0.25, 0.0)
+            meta.isHasGlowingEffect = true
+        }
+        player.displayName = component
+        player.addPassenger(display)
+    }
+}
