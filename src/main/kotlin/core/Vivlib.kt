bf85183fe34b5ecd248466fc5cc2c8feb7994bb4
aquestry <antonkonig691@gmail.com>
2025-06-13 14:33:00 +0200

    move more logic to vivlib

--- src/main/kotlin/core/Vivlib.kt
+++ src/main/kotlin/core/Vivlib.kt
@@ -1,6 +1,8 @@
 package core
 
 import net.eupixel.vivlib.command.WhereAmICommand
+import net.eupixel.vivlib.core.DBTranslator
+import net.eupixel.vivlib.core.DirectusClient
 import net.eupixel.vivlib.core.Messenger
 import net.eupixel.vivlib.core.WhitelistManager
 import net.minestom.server.MinecraftServer
@@ -8,6 +10,9 @@ import net.minestom.server.event.player.AsyncPlayerConfigurationEvent
 
 object Vivlib {
     fun init() {
+        WhitelistManager.start()
+        DirectusClient.initFromEnv()
+        DBTranslator.loadFromDB()
         Messenger.addWhiteListListener()
         MinecraftServer.getCommandManager().register(WhereAmICommand())
         MinecraftServer.getGlobalEventHandler().addListener(AsyncPlayerConfigurationEvent::class.java) { event -> WhitelistManager.handle(event) }
